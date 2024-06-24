da2481b677398047160bde2bfa26ef5fffb55668
Ederson Caldato <168448799+edersoncaldato@users.noreply.github.com>
2024-06-24 15:07:01 -0300

    Jogador Alterado
    IA para que seja feita a melhor jogada

--- src/Jogador.java
+++ src/Jogador.java
@@ -1,12 +1,13 @@
 package src;
 
-import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class Jogador {
 
     public List<Carta> cartas;
-
     public String nome;
 
     public Jogador(String nome) {
@@ -14,16 +15,52 @@ public class Jogador {
         this.nome = nome;
     }
 
-    public Carta jogada(Carta cartaNaMesa){
-        return cartas.get(0);
+    public Carta jogada(Carta cartaNaMesa) {
+        return jogar(cartaNaMesa);
     }
 
+    private Carta jogar(Carta cartaNaMesa) {
+        if (cartas.size() == 3) {
+            Optional<Carta> optionalCartaAlta = cartas.stream()
+                    .max(Comparator.comparingInt(c -> c.valor));
+            if (cartaNaMesa == null) {
+                Carta cartaAlta = optionalCartaAlta.get();
+                if (cartaAlta.valor >= 130 && cartaAlta.valor <= 130000) {
+                    return cartaAlta;
+                }
+            }
+            return cartas.stream()
+                    .max(Comparator.comparingInt(c -> c.numero))
+                    .orElse(null);
 
+        } else if (cartas.size() == 2 || cartas.size() == 1) {
+            return getCarta();
+        } else {
+            System.err.println("Jogador ta com muita carta: " + cartas.size());
+            return null;
+        }
+    }
 
-    public void exibeCartas(){
-        cartas.forEach(c -> {
-            System.out.print(c + " ");
-        });
+    private Carta getCarta() {
+        Optional<Carta> optionalCartaAlta = cartas.stream()
+                .max(Comparator.comparingInt(c -> c.valor));
+        if (optionalCartaAlta.isPresent()) {
+            Carta cartaAlta = optionalCartaAlta.get();
+            if (cartaAlta.valor >= 130 && cartaAlta.valor <= 130000) {
+                return cartaAlta;
+            } else {
+                return cartas.stream()
+                        .max(Comparator.comparingInt(c -> c.numero))
+                        .orElse(null);
+            }
+        } else {
+            System.err.println("Jogador ta sem carta pra jogar!");
+            return null;
+        }
+    }
+
+    public void exibeCartas() {
+        cartas.forEach(c -> System.out.print(cartas + " "));
         System.out.println();
     }
 }
